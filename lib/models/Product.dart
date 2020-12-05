3e14f1d41acb2400076da1feb4f11e2d1bd98c9e
Rahmad Hidayat Rambe <rahmadhr98@gmail.com>
2020-12-05 22:49:44 +0700

    Randi Hidayat

---
+++ lib/models/Product.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final int id;
+  final String title, description;
+  final List<String> images;
+  final List<Color> colors;
+  final double rating, price;
+  final bool isFavourite, isPopular;
+
+  Product({
+    @required this.id,
+    @required this.images,
+    @required this.colors,
+    this.rating = 0.0,
+    this.isFavourite = false,
+    this.isPopular = false,
+    @required this.title,
+    @required this.price,
+    @required this.description,
+  });
+}
+
+// Our demo Products
+
+List<Product> demoProducts = [
+  Product(
+    id: 1,
+    images: [
+      "assets/images/tenda1.png",
+      "assets/images/tenda2.jpg",
+      "assets/images/tenda3.jpg",
+      "assets/images/tenda4.jpg",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Pelaminan Klasik Kekinian",
+    price: 0.0,
+    description: description,
+    rating: 4.8,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 2,
+    images: [
+      "assets/images/henna1.jpg",
+      "assets/images/henna2.jpg",
+      "assets/images/henna3.jpg",
+      "assets/images/henna4.jpg",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Henna Art",
+    price: 0.0,
+    description: description,
+    rating: 4.1,
+    isPopular: true,
+  ),
+  Product(
+    id: 3,
+    images: [
+      "assets/images/pakaian1.jpg",
+      "assets/images/pakaian2.jpg",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Pakaian pengantin",
+    price: 0.0,
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+    isPopular: true,
+  ),
+  Product(
+    id: 4,
+    images: [
+      "assets/images/kompang1.jpg",
+      "assets/images/kompang2.jpg",
+    ],
+    colors: [
+      Color(0xFFF6625E),
+      Color(0xFF836DB8),
+      Color(0xFFDECB9C),
+      Colors.white,
+    ],
+    title: "Kompang",
+    price: 20.20,
+    description: description,
+    rating: 4.1,
+    isFavourite: true,
+  ),
+];
+
+const String description =
+    "Pelaminan yang sangat bagus dan elegan banget";
